8c831ae8fbdeb27dd26f02637a92b88cff2beb2c
Brijesh Maurya <brijeshmauriya4578@gmail.com>
2025-08-30 19:30:52 +0530

    Initial commit - Stock Trading Platform

---
+++ backend/model/HoldingsModel.js
@@ -0,0 +1,10 @@
+
+const { model} = require("mongoose");
+
+const {HoldingsSchema} = require("../schemas/HoldingsSchema");
+
+const HoldingsModel =  model("holding", HoldingsSchema);
+
+module.exports = { HoldingsModel };
+
+
