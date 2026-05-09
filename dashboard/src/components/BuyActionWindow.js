57aa8a886a22a79d7af4774753457ff72bfc6481
Brijesh Maurya <brijeshmauriya4578@gmail.com>
2026-05-09 13:12:50 +0530

    update backend URLs

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -12,7 +12,7 @@ const BuyActionWindow = ({ uid }) => {
   const [stockPrice, setStockPrice] = useState(0.0);
 
   const handleBuyClick = () => {
-    axios.post("http://localhost:3002/newOrder", {
+    axios.post("https://stock-trading-platform-844w.onrender.com/newOrder", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
